22db5b36134c305804135bb324cee16a3c345193
Izanderson Florencio <izandersonf@gmail.com>
2024-06-17 18:41:17 -0300

    feat: add resend as email provider

--- config/plugins.js
+++ config/plugins.js
@@ -26,4 +26,16 @@ module.exports = ({ env }) => ({
       },
     },
   },
+  email: {
+    config: {
+      provider: 'strapi-provider-email-resend',
+      providerOptions: {
+        apiKey: env('RESEND_API_KEY'),
+      },
+      settings: {
+        defaultFrom: 'oi@gdg.natal.br',
+        defaultReplyTo: 'oi@gdg.natal.br',
+      },
+    }
+  }
 });
